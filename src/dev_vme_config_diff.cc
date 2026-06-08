73c875008cb41afbdd9a47e66d38885b1947bdab
Florian Lüke <f.lueke@mesytec.com>
2026-06-08 12:19:53 +0200

    vme_config_diff: fix the 'no changes' test
    The AI did not or could not see that ModuleConfig objects have child
    scripts added to them at construction time. These child objects will get
    their own unique ids every time the test runs. The fix is to manually
    reassign the ids to known values.

--- src/dev_vme_config_diff.cc
+++ src/dev_vme_config_diff.cc
@@ -43,6 +43,8 @@ std::unique_ptr<VMEConfig> create_test_config_1()
     event1->setObjectName("Event0");
     event1->irqLevel = 1;
     event1->irqVector = 0;
+    event1->setVariable("var1", vme_script::Variable("value1", "This is test variable 1"));
+    event1->setVariable("var2", vme_script::Variable("value2", "This is test variable 2"));
     vmeConfig->addEventConfig(event1);
 
     // Add a module to the event
@@ -50,12 +52,17 @@ std::unique_ptr<VMEConfig> create_test_config_1()
     module1->setId(QUuid::fromString(QSL("{00000000-0000-0000-0000-000000000003}")));
     module1->setObjectName("Module0");
     module1->setBaseAddress(0x10000000);
-
     vats::VMEModuleMeta meta;
     meta.typeName = "mdpp16_scp";
+    module1->setModuleMeta(meta);
+    module1->getResetScript()->setId(QUuid::fromString(QSL("{00000000-0000-0000-0000-000000000006}")));
+    module1->getReadoutScript()->setId(QUuid::fromString(QSL("{00000000-0000-0000-0000-000000000007}")));
+
     // Add init script to module
     auto initScript = new VMEScriptConfig("init", "# Init script\nwrite a32 d16 0x6008 1\n");
+    initScript->setId(QUuid::fromString(QSL("{00000000-0000-0000-0000-000000000004}")));
     module1->addInitScript(initScript);
+    event1->addModuleConfig(module1);
 
     return vmeConfig;
 }
@@ -74,6 +81,8 @@ std::unique_ptr<VMEConfig> create_test_config_2()
     event1->setObjectName("Event0");
     event1->irqLevel = 2;  // Changed!
     event1->irqVector = 0;
+    event1->setVariable("var1", vme_script::Variable("value1", "This is test variable 1"));
+    event1->setVariable("var2", vme_script::Variable("value3", "This is test variable 2"));
     vmeConfig->addEventConfig(event1);
 
     // Same module with different address
@@ -81,15 +90,20 @@ std::unique_ptr<VMEConfig> create_test_config_2()
     module1->setId(QUuid::fromString(QSL("{00000000-0000-0000-0000-000000000003}")));
     module1->setObjectName("Module0");
     module1->setBaseAddress(0x20000000);  // Changed!
-
     vats::VMEModuleMeta meta;
     meta.typeName = "mdpp16_scp";
+    module1->setModuleMeta(meta);
+    module1->getResetScript()->setId(QUuid::fromString(QSL("{00000000-0000-0000-0000-000000000006}")));
+    module1->getReadoutScript()->setId(QUuid::fromString(QSL("{00000000-0000-0000-0000-000000000007}")));
+
     auto initScript = new VMEScriptConfig("init", "# Init script\nwrite a32 d16 0x6008 2\n");  // Changed!
+    initScript->setId(QUuid::fromString(QSL("{00000000-0000-0000-0000-000000000004}")));
     module1->addInitScript(initScript);
+    event1->addModuleConfig(module1);
 
     // Add a second module (new!)
     auto module2 = new ModuleConfig();
-    module2->setId(QUuid::fromString(QSL("{00000000-0000-0000-0000-000000000004}")));
+    module2->setId(QUuid::fromString(QSL("{00000000-0000-0000-0000-000000000005}")));
     module2->setObjectName("Module1");
     module2->setBaseAddress(0x30000000);
     module2->setModuleMeta(meta);
